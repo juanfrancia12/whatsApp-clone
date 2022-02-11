a365a10b541cbc29a84a2c51aa4b85030206159b
juanfrancia12 <jf.developer01@gmail.com>
2022-02-11 16:18:53 -0500

    Agregado CDN

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 const path = require("path");
-const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd
-    ? "https://cdn.statically.io/gh/juanfrancia12/juanfrancia12.github.io/gh-pages/"
-    : "",
 };
