621980c3e4b914fd88534dde4d72999070631bf5
juanfrancia12 <jf.developer01@gmail.com>
2022-02-11 16:14:29 -0500

    Agregado CDN

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    // Use the CDN in production and localhost for development.
-    assetPrefix: isProd
-      ? "https://cdn.statically.io/gh/juanfrancia12/juanfrancia12.github.io/gh-pages/"
-      : "",
   },
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/juanfrancia12/juanfrancia12.github.io/gh-pages/"
+    : "",
 };
