14ebfa679f882d01e6cc510959795c904a423685
juanfrancia12 <jf.developer01@gmail.com>
2022-02-11 16:01:36 -0500

    Agregado CD

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const path = require("path");
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    // Use the CDN in production and localhost for development.
+    assetPrefix: isProd
+      ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
+      : "",
   },
 };
